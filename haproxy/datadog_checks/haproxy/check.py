5360944a996393dae53ea758947cce936b1cb96c
Ofek Lev <ofekmeister@gmail.com>
2020-09-16 17:48:20 -0400

    Add new implementation

--- haproxy/datadog_checks/haproxy/check.py
+++ haproxy/datadog_checks/haproxy/check.py
@@ -1,12 +1,15 @@
 # (C) Datadog, Inc. 2020-present
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
-from datadog_checks.base import AgentCheck, is_affirmative
+from datadog_checks.base import OpenMetricsBaseCheck, is_affirmative
 
 from .legacy.haproxy import HAProxyCheckLegacy
+from .metrics import METRIC_MAP
 
 
-class HAProxyCheck(AgentCheck):
+class HAProxyCheck(OpenMetricsBaseCheck):
+    DEFAULT_METRIC_LIMIT = 0
+
     def __new__(cls, name, init_config, instances):
         instance = instances[0]
 
@@ -16,7 +19,19 @@ class HAProxyCheck(AgentCheck):
             return super(HAProxyCheck, cls).__new__(cls)
 
     def __init__(self, name, init_config, instances):
-        super(HAProxyCheck, self).__init__(name, init_config, instances)
-
-    def check(self, _):
-        raise NotImplementedError()
+        super(HAProxyCheck, self).__init__(
+            name,
+            init_config,
+            instances,
+            default_instances={
+                'haproxy': {
+                    'prometheus_url': 'http://localhost:8404/metrics',
+                    'namespace': 'haproxy',
+                    'metrics': [METRIC_MAP],
+                    'send_histograms_buckets': True,
+                    'send_distribution_counts_as_monotonic': True,
+                    'send_distribution_sums_as_monotonic': True,
+                }
+            },
+            default_namespace='haproxy',
+        )
